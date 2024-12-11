119dcdab4231e8157aec3ffe1999807e16a62788
Azizul Hakim <148012067+azizulhakim6817@users.noreply.github.com>
2024-12-11 14:04:36 +0600

    Navber resposive design

--- client/src/commponent/layout/AppNavbar.jsx
+++ client/src/commponent/layout/AppNavbar.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
 import UserStore from "../../../store/UserStore";
-import UserSubmitButton from "./../users/UserSubmitButton";
 import Cookies from "js-cookie";
 
 const AppNavbar = () => {
-  let navigate = useNavigate();
-
+  const navigate = useNavigate();
   const { isLogin, UserLogoutequest } = UserStore();
 
   const onLogout = async () => {
@@ -24,74 +22,74 @@ const AppNavbar = () => {
 
   return (
     <>
-      <div className="container-fluid text-white p-2 bg-secondary">
+      {/* Top Bar */}
+      <div className="container-fluid bg-secondary text-white p-2">
         <div className="container">
-          <div className="row justify-content-around">
-            <div className="col-md-6">
-              <span>
-                <span className="f-12">
-                  <i className="bi bi-envelope"></i>
-                  <a
-                    href="mailto:azizulhakim68178@gmail.com"
-                    className="ms-2 text-white text-decoration-none fw-bold"
-                  >
-                    azizulhakim68178@gmail.com
-                  </a>
-                </span>
-                <span className="f-12 mx-2">
-                  <i className="bi bi-phone"></i>
-                  <a
-                    href="tel:01743086886"
-                    className="ms-2 text-white fw-bold text-decoration-none"
-                  >
-                    01743-086886
-                  </a>
-                </span>
-              </span>
+          <div className="row align-items-center">
+            {/* Contact Details */}
+            <div className="col-md-6 d-flex justify-content-start">
+              <div className="me-3">
+                <i className="bi bi-envelope"></i>
+                <a
+                  href="mailto:azizulhakim68178@gmail.com"
+                  className="ms-2 text-white text-decoration-none fw-bold"
+                >
+                  azizulhakim68178@gmail.com
+                </a>
+              </div>
+              <div>
+                <i className="bi bi-phone"></i>
+                <a
+                  href="tel:01743086886"
+                  className="ms-2 text-white text-decoration-none fw-bold"
+                >
+                  01743-086886
+                </a>
+              </div>
             </div>
-            <div className="col-md-6">
-              <span className="float-end">
-                <span className="bodySmal mx-2">
-                  <i className="bi bi-whatsapp"></i>
-                </span>
-                <span className="bodySmal mx-2">
-                  <i className="bi bi-youtube"></i>
-                </span>
-                <span className="bodySmal">
-                  <i className="bi bi-facebook"></i>
-                </span>
-              </span>
+            {/* Social Media Icons */}
+            <div className="col-md-6 d-flex justify-content-md-end mt-2 mt-md-0">
+              <a href="#" className="text-white mx-2">
+                <i className="bi bi-whatsapp"></i>
+              </a>
+              <a href="#" className="text-white mx-2">
+                <i className="bi bi-youtube"></i>
+              </a>
+              <a href="#" className="text-white">
+                <i className="bi bi-facebook"></i>
+              </a>
             </div>
           </div>
         </div>
       </div>
 
       {/* Navbar */}
-      <nav className="navbar sticky-top shadow-sm bg-white navbar-expand-lg navbar-light">
-        <div className="container d-flex justify-content- align-items-center p-0">
+      <nav className="navbar sticky-top navbar-expand-lg bg-white shadow-sm">
+        <div className="container">
+          {/* Logo */}
+          <Link to="/" className="navbar-brand">
+            <img src={logo} alt="Logo" width="96px" className="img-fluid" />
+          </Link>
+
+          {/* Navbar Toggle Button */}
           <button
             className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
-            data-bs-target="#navbarTogglerDemo01"
-            aria-controls="navbarTogglerDemo01"
+            data-bs-target="#navbarToggler"
+            aria-controls="navbarToggler"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div className="navbar-brand">
-            <Link to="/">
-              <img className="img-fluid" src={logo} alt="Logo" width="96px" />
-            </Link>
-          </div>
-
+          {/* Navbar Links */}
           <div
-            className="collapse navbar-collapse text-end"
-            id="navbarTogglerDemo01"
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarToggler"
           >
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav">
               <li className="nav-item">
                 <Link className="nav-link active" to="/">
                   Home
